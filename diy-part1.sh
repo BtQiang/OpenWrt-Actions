419d91cce2f81a799b75759b7bb6166279966e08
BtQiang <baby-0521@qq.com>
2021-04-20 21:59:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,23 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+#更改时区
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
+sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
+
+#自定插件
+git clone https://github.com/jerrykuku/luci-app-ttnode.git
+
+#更新feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
